acb161403eab99d461d264e1dfcec5f017d2782c
demasiadoblues <92554315+demasiadoblues@users.noreply.github.com>
2023-06-01 19:17:44 -0300

    cambios varios
    elimine un boton,modifique el script para agregar el boton con la lista de opciones pero todavia no funciona.

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-function showAlert() { alert("hola mundo,alerta de javascript");}
+function showAlert() { alert("hola mundo,alerta de javascript"); }
 //obtenemos el boton por su id//
-let boton= document.getElementById("miBoton");
-//añadimos un oyente de eventos que escuche el evento "click"//
-boton.addEventListener("click",function(){
-    alert("presionaste el boton!!");
-})
+let boton = document.getElementById("miBoton");
 
 const form = document.getElementById('myForm');
-form.addEventListener('submit', function(event) {
-event.preventDefault();
-validateForm();
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    validateForm();
 })
 function validateEmail(email) {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
     return regex.test(email)
-    }
-    function validateForm() {
+}
+function validateForm() {
     const emailInput = document.getElementById('email');
     const email = emailInput.value;
     if (!validateEmail(email)) {
-    alert('Por favor ingrese un correo electrónico válido.');
+        alert('Por favor ingrese un correo electrónico válido.');
     } else {
-    alert('Correo electrónico enviado correctamente.');
+        alert('Correo electrónico enviado correctamente.');
     }
-    }
-    
+}
+//accionar la barra
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function () {
+        document.querySelector(".nav-links").
+            classList.toggle("nav-links-responsive")
+    })
